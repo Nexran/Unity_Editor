1fe5fd8fa448278cb514fb668bc91b376656ed1b
Chad Arr <chadarr@gmail.com>
2016-06-16 14:15:19 -0700

    Code clean up

--- Assets/Scripts/Attributes/PhoneAttribute.cs
+++ Assets/Scripts/Attributes/PhoneAttribute.cs
@@ -2,8 +2,8 @@
 
 public class PhoneAttribute : RegexAttribute 
 {
-	private static string pattern = @"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}";
-	private static string message = "Invalid Phone number! Example: '555-555-5555'";
+	private static string _pattern = @"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}";
+	private static string _message = "Invalid Phone number! Example: '555-555-5555'";
 
-	public PhoneAttribute() : base(pattern, message)	{	}
+	public PhoneAttribute() : base(_pattern, _message)	{	}
 }
--- Assets/Scripts/Attributes/URLAttribute.cs
+++ Assets/Scripts/Attributes/URLAttribute.cs
@@ -2,8 +2,8 @@
 
 public class URLAttribute : RegexAttribute 
 {
-	private static string pattern = @"^(http|https|ftp|)\://|[a-zA-Z0-9\-\.]+\.[a-zA-Z](:[a-zA-Z0-9]*)?/?([a-zA-Z0-9\-\._\?\,\'/\\\+&amp;%\$#\=~])*[^\.\,\)\(\s]$";
-	private static string message = "Invalid URL address! Example: 'http://www.google.com'";
+	private static string _pattern = @"^(http|https|ftp|)\://|[a-zA-Z0-9\-\.]+\.[a-zA-Z](:[a-zA-Z0-9]*)?/?([a-zA-Z0-9\-\._\?\,\'/\\\+&amp;%\$#\=~])*[^\.\,\)\(\s]$";
+	private static string _message = "Invalid URL address! Example: 'http://www.google.com'";
 
-	public URLAttribute() : base(pattern, message)	{	}
+	public URLAttribute() : base(_pattern, _message)	{	}
 }
